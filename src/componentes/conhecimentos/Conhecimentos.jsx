255758db38f3478b3fc99a25b889d63501c5f742
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-08-18 01:00:07 -0300

    Agora sim está finalizado o css de conhecimentos

--- src/componentes/conhecimentos/Conhecimentos.jsx
+++ src/componentes/conhecimentos/Conhecimentos.jsx
@@ -11,21 +11,21 @@ const Conhecimentos = () => {
             <p>Aqui vai uma lista das tecnologias que estou mais familiarizado.</p>
             <div className="listas-conhecimentos">
                 <ul className="start-lista">
-                    <li><img src="./img/html.png" alt="html icone" width="25px"/> HTML</li>
-                    <li><img src="./img/css.png" alt="css icone" width="25px"/> CSS</li>
-                    <li><img src="./img/javascript.png" alt="javaScript icone" width="25px"/> JavaScript</li>
-                    <li><img src="./img/javascript.png" alt="javaScript icone" width="25px"/> NodeJS</li>
-                    <li><img src="./img/atom.png" alt="react icone" width="25px"/> React</li>
-                    <li><img src="./img/java.png" alt="java icone" width="25px"/> Java</li>
+                    <li><img src="./img/html.png" alt="html icone"/><span>HTML</span></li>
+                    <li><img src="./img/css.png" alt="css icone"/><span>CSS</span></li>
+                    <li><img src="./img/javascript.png" alt="javaScript icone"/><span>JavaScript</span></li>
+                    <li><img src="./img/javascript.png" alt="javaScript icone"/><span>NodeJS</span></li>
+                    <li><img src="./img/atom.png" alt="react icone"/><span>React</span></li>
+                    <li><img src="./img/java.png" alt="java icone"/><span>Java</span></li>
                 </ul>
                 <ul className="end-lista">
-                    <li><img src="./img/python.png" alt="python icone" width="25px"/> Python</li>
-                    <li><img src="./img/git.png" alt="git icone" width="25px"/> Git</li>
-                    <li><img src="./img/nodered.png" alt="nodered icone" width="25px"/> Nodered</li>
-                    <li><img src="./img/chatbot.png" alt="chatbot icone" width="25px"/> IBM cloud (Watson Assistant)</li>
-                    <li><img src="./img/sql.png" alt="sql icone" width="25px"/> SQL</li>
-                    <li><img src="./img/database.png" alt="database icone" width="25px"/> Oracle DB</li>
-                    <li><img src="./img/datamodelling.png" alt="data modelling icone" width="25px"/> Modelagem de banco de dados</li>
+                    <li><img src="./img/python.png" alt="python icone"/><span>Python</span></li>
+                    <li><img src="./img/git.png" alt="git icone"/><span>Git</span></li>
+                    <li><img src="./img/nodered.png" alt="nodered icone"/><span>Nodered</span></li>
+                    <li><img src="./img/chatbot.png" alt="chatbot icone"/><span>IBM cloud (Watson Assistant)</span></li>
+                    <li><img src="./img/sql.png" alt="sql icone"/><span>SQL</span></li>
+                    <li><img src="./img/database.png" alt="database icone"/><span>Oracle DB</span></li>
+                    <li><img src="./img/datamodelling.png" alt="data modelling icone"/><span>Modelagem de banco de dados</span></li>
                 </ul>
             </div>
         </section>
