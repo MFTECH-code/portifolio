cd9968b4206c9c1620cd77df3436742b24589334
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-08-18 13:07:36 -0300

    Estilização de redes pronta

--- src/componentes/redes/Redes.jsx
+++ src/componentes/redes/Redes.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import "./redes.css";
 
 const Redes = () => {
     return (
-        <section>
+        <section className="redes">
             <h1>Me siga nas redes sociais!</h1>
+            <div className="img-redes">
+                <img src="./img/redes.jpg" alt="minhas redes sociais" />
+            </div>
             <ul>
-                <li><a href="https://www.linkedin.com/in/matheus-feitosa-6684381b3/" target="_blank" rel="noreferrer">Linkedin</a></li>
-                <li><a href="https://github.com/MFTECH-code" target="_blank" rel="noreferrer">GitHub</a></li>
-                <li><a href="https://www.instagram.com/silvamatheusfeitosa/" target="_blank" rel="noreferrer">Instagram</a></li>
+                <li><a href="https://www.linkedin.com/in/matheus-feitosa-6684381b3/" target="_blank" rel="noreferrer"><img src="./img/linkedin.png" alt="linkedin icone" /><span>Linkedin</span></a></li>
+                <li><a href="https://github.com/MFTECH-code" target="_blank" rel="noreferrer"><img src="./img/github.png" alt="github icone" /><span>GitHub</span></a></li>
+                <li><a href="https://www.instagram.com/silvamatheusfeitosa/" target="_blank" rel="noreferrer"><img src="./img/instagram.png" alt="instagram icone" /><span>Instagram</span></a></li>
             </ul>
         </section>
     );
