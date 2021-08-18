93e66efd923ccc7741c2456c571d12783dd98da3
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-08-18 12:43:09 -0300

    Estilização experiencias concluida

--- src/componentes/experiencias/Experiencias.jsx
+++ src/componentes/experiencias/Experiencias.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import "./experiencias.css";
 
 const Experiencias = () => {
     return (
-        <section>
+        <section className="experiencias">
             <h1>Minhas experiências</h1>
+            <div className="img-experiencias">
+                <img src="./img/experiencias.jpg" alt="minhas experiências" />
+            </div>
             <ul>
-                <li>Atendimento ao cliente e vendas | Liq | 2021 - Atual Emprego</li>
-                <li>Participação de game JAM | None JAM | 2020</li>
-                <li>Projeto Gulliver Global Solution | FIAP | 2021</li>
-                <li>ZenSpaceOn | FIAP | 2021</li>
+                <li><a href="https://www.liq.com.br/" target="_blank" rel="noreferrer"><img src="./img/atendente.png" alt="atendente icone" /><span>Atendimento ao cliente e vendas | Liq | 2021 - Atual Emprego</span></a></li>
+                <li><a href="https://itch.io/jam/proficionaljam/rate/735882" target="_blank" rel="noreferrer"><img src="./img/game.png" alt="game icone" /><span>Participação de game JAM | None JAM | 2020</span></a></li>
+                <li><a href="https://github.com/ZenGroup-FIAP/GlobalSolution2021.1" target="_blank" rel="noreferrer"><img src="./img/project.png" alt="project icone" /><span>Projeto Gulliver Global Solution | FIAP | 2021</span></a></li>
+                <li><a href="https://github.com/ZenGroup-FIAP/projeto-ibm-zenspaceon" target="_blank" rel="noreferrer"><img src="./img/project.png" alt="project icone" /><span>ZenSpaceOn | FIAP | 2021</span></a></li>
             </ul>
         </section>
     );
