f434cfa90e71f4c97e77d9ef4a5af47d082539a9
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-08-18 12:25:16 -0300

    Estilização formação concluída

--- src/componentes/formacao/Formacao.jsx
+++ src/componentes/formacao/Formacao.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import "./formacao.css";
 
 const Formacao = () => {
     return (
-        <section>
+        <section className="formacao">
             <h1>Minha formação</h1>
+            <div className="img-formacao">
+                <img src="./img/formacao.jpg" alt="formação"/>
+            </div>
             <ul>
-                <li>Análise e desenvolvimento de sistemas | FIAP | 2021 - 2022</li>
-                <li>Ensino médio completo | Colégio São Judas Tadeu | 2018 - 2020</li>
+                <li><img src="./img/graduate.png" alt="icone graduação" /><span>Análise e desenvolvimento de sistemas | FIAP | 2021 - 2022</span></li>
+                <li><img src="./img/graduate.png" alt="icone graduação" /><span>Ensino médio completo | Colégio São Judas Tadeu | 2018 - 2020</span></li>
             </ul>
             <h2>Certificados</h2>
             <ul>
-                <li>Java Fundamentos | Nano Course FIAP | 2021</li>
-                <li>Autoridade em algorítimos | DevMedia | 2021</li>
-                <li>Autoridade em javaScript | DevMedia | 2021</li>
-                <li>Formação Social e Sustentabilidade | Nano course FIAP | 2021</li>
+                <li><img src="./img/certificate.png" alt="icone certificado" /><span>Java Fundamentos | Nano Course FIAP | 2021</span></li>
+                <li><img src="./img/certificate.png" alt="icone certificado" /><span>Autoridade em algorítimos | DevMedia | 2021</span></li>
+                <li><img src="./img/certificate.png" alt="icone certificado" /><span>Autoridade em javaScript | DevMedia | 2021</span></li>
+                <li><img src="./img/certificate.png" alt="icone certificado" /><span>Formação Social e Sustentabilidade | Nano course FIAP | 2021</span></li>
             </ul>
         </section>
     );
