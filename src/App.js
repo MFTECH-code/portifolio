3d5f912bae378799b019ba69c548581bce02c6e8
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-09-26 00:17:11 -0300

    Configurando rotas com router

--- src/App.js
+++ src/App.js
@@ -1,63 +1,10 @@
-import React, {useState} from "react";
 import Cabecalho from "./componentes/cabecalho/Cabecalho";
-import MinhaHistoria from "./componentes/minha_historia/MinhaHistoria";
-import Conhecimentos from "./componentes/conhecimentos/Conhecimentos";
-import Formacao from "./componentes/formacao/Formacao";
-import Experiencias from "./componentes/experiencias/Experiencias";
-import Redes from "./componentes/redes/Redes";
 import "./App.css";
 
 export default function App() {
-
-  const [minhaHistoria, setMinhaHistoria] = useState(false);
-  const [conhecimentos, setConhecimentos] = useState(false);
-  const [formacao, setFormacao] = useState(false);
-  const [experiencias, setExperiencias] = useState(false);
-  const [redes, setRedes] = useState(false);
-
-  const mostraConteudo = () => {
-    if (minhaHistoria) {
-      return <MinhaHistoria/>
-    } else if (conhecimentos) {
-      return <Conhecimentos/>
-    } else if (formacao) {
-      return <Formacao/>
-    } else if (experiencias) {
-      return <Experiencias/>
-    } else if (redes){
-      return <Redes/>
-    } else {
-      return(
-        <section className="bem-vindo">
-          <h1 className="ola">Olá!</h1>
-          <img src="./img/ola.jpg" alt="Saudação" />
-          <div className="texto">
-            <p>Seja bem vindo ao meu portifólio, onde eu apresento um resumo da minha história, conhecimentos e experiências.</p>
-            <p>E sim eu sou muito fã do Jackie Chan, você vai ver várias imagens dele por aqui &#x1F604;.</p>
-            </div>
-          <h1>Agradecimentos</h1>
-          <div className="texto">
-              <p className="texto-agradecimentos">Primeiramente agradeço o meu pai que nunca desistiu de me dar uma educação de qualidade, mesmo quando me mostrei não merecedor ele sempre me apoiou com seus sábios conselhos. Que ele descanse em paz &#x1F64F;.</p>
-          </div>
-        </section>
-      )
-    }
-  }
-
   return (
     <section className="app">
-      <Cabecalho 
-        minhaHistoria={minhaHistoria}
-        conhecimentos={conhecimentos}
-        formacao={formacao}
-        experiencias={experiencias}
-        redes={redes}
-        setMinhaHistoria={setMinhaHistoria}
-        setConhecimentos={setConhecimentos}
-        setFormacao={setFormacao}
-        setExperiencias={setExperiencias}
-        setRedes={setRedes}/>
-        {mostraConteudo()}
+      <Cabecalho />
     </section>
   );
 }
--- src/componentes/cabecalho/Cabecalho.jsx
+++ src/componentes/cabecalho/Cabecalho.jsx
@@ -1,63 +1,50 @@
 import React from 'react';
+import MinhaHistoria from "../minha_historia/MinhaHistoria";
+import Conhecimentos from "../conhecimentos/Conhecimentos";
+import Formacao from "../formacao/Formacao";
+import Experiencias from "../experiencias/Experiencias";
+import Redes from "../redes/Redes";
+import {Link, Switch, Route} from "react-router-dom";
 import "./cabecalho.css";
 
-const Cabecalho = (props) => {
-
-    const trocaEstadoMinhaHistoria = () => {
-        props.setMinhaHistoria(!props.minhaHistoria)
-        props.setConhecimentos(false)
-        props.setExperiencias(false)
-        props.setFormacao(false)
-        props.setRedes(false)
-    }
-
-    const trocaEstadoConhecimentos = () => {
-        props.setConhecimentos(!props.conhecimentos)
-        props.setMinhaHistoria(false)
-        props.setExperiencias(false)
-        props.setFormacao(false)
-        props.setRedes(false)
-    }
-
-    const trocaEstadoFormacao = () => {
-        props.setFormacao(!props.formacao)
-        props.setMinhaHistoria(false)
-        props.setExperiencias(false)
-        props.setConhecimentos(false)
-        props.setRedes(false)
-    }
-
-    const trocaEstadoExperiencias = () => {
-        props.setExperiencias(!props.experiencias)
-        props.setMinhaHistoria(false)
-        props.setFormacao(false)
-        props.setConhecimentos(false)
-        props.setRedes(false)
-    }
-
-    const trocaEstadoRedes = () => {
-        props.setRedes(!props.redes)
-        props.setMinhaHistoria(false)
-        props.setFormacao(false)
-        props.setConhecimentos(false)
-        props.setExperiencias(false)
-    }
-
-
+const Cabecalho = () => {
 
     return (
-        <header className="cabecalho">
-            <h1>Olá, meu nome é Matheus...</h1>
-            <nav className="navBar">
-                <ul>
-                    <li><a href="#minhaHistoria" onClick={() => trocaEstadoMinhaHistoria()}>Minha História</a></li>
-                    <li><a href="#conhecimentos" onClick={() => trocaEstadoConhecimentos()}>Conhecimentos</a></li>
-                    <li><a href="#formacao" onClick={() => trocaEstadoFormacao()}>Formação</a></li>
-                    <li><a href="#experiencias" onClick={() => trocaEstadoExperiencias()}>Experiências</a></li>
-                    <li><a href="#redesSociais" onClick={() => trocaEstadoRedes()}>Redes Sociais</a></li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <header className="cabecalho">
+                <Link to="/">
+                    <h1>Olá, meu nome é Matheus...</h1>
+                </Link>
+                <nav className="navBar">
+                    <ul>
+                        <li><Link to="/conhecimentos">Conhecimentos</Link></li>
+                        <li><Link to="/formacao">Formacão</Link></li>
+                        <li><Link to="/experiencias">Experiências</Link></li>
+                        <li><Link to="/redes">Redes</Link></li>
+                        <li></li>
+                    </ul>
+                </nav>
+            </header>
+            <section>
+                <Switch>
+                    <Route path="/conhecimentos">
+                        <Conhecimentos />
+                    </Route>
+                    <Route path="/formacao">
+                        <Formacao />
+                    </Route>
+                    <Route path="/experiencias">
+                        <Experiencias />
+                    </Route>
+                    <Route path="/redes">
+                        <Redes />
+                    </Route>
+                    <Route path="/">
+                        <MinhaHistoria />
+                    </Route>
+                </Switch>
+            </section>
+        </>
     );
 }
 
